cfe2afb7b6ce1f8a25c653c70a992c52387cc465
Jithesh E J <mail@jithesh.net>
2016-07-12 20:05:34 +0530

    Adding item with the necessary fields(Images Pipeline)

--- myntra/items.py
+++ myntra/items.py
@@ -5,10 +5,9 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class MyntraItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MyntraItem(Item):
+    image_urls = Field()
+    images = Field()
