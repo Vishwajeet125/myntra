833de70b4be314f5053ad6adb79d7f13e1399deb
Jithesh E J <mail@jithesh.net>
2016-07-12 20:06:40 +0530

    Overriding start_requests

--- myntra/spiders/images.py
+++ myntra/spiders/images.py
@@ -1,13 +1,33 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Request, Spider
+
+from os.path import abspath, dirname, join
+from string import strip
 
 
-class ImagesSpider(scrapy.Spider):
+class ImagesSpider(Spider):
     name = "images"
     allowed_domains = ["myntra.com"]
-    start_urls = (
-        'http://www.myntra.com/',
-    )
+
+    def start_requests(self):
+        base_url = 'http://www.myntra.com/search-service/searchservice/search/filteredSearch'
+        # Read in styleid(s)
+        with open(join(dirname(abspath(__file__)), 'ids.txt'), 'r') as f:
+            style_ids = map(strip, f)
+        # Raise NameError if `styleids` is not present
+        if not style_ids:
+            raise NameError('Make sure ids.txt exists and has valid entries!')
+
+        for style_id in style_ids:
+            payload = '[{"fq":["count_options_availbale:[1 TO *]"],' \
+                      '"query":"styleid : %s AND  (count_options_availbale:[' \
+                      '0 TO *])",' \
+                      '"start":0,' \
+                      '"rows":15,' \
+                      '"return_docs":true,' \
+                      '"facet":false,' \
+                      '"sort":[]}]' % style_id
+            yield Request(url=base_url, method='POST', body=payload)
 
     def parse(self, response):
         pass
