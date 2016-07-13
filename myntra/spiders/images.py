89fea374c8ad473f6c82e36fba0b9d316fbf989c
Jithesh <mail@jithesh.net>
2016-07-14 02:28:50 +0530

    Getting all the text(not just the first element of the list)

--- myntra/spiders/images.py
+++ myntra/spiders/images.py
@@ -57,7 +57,7 @@ class ImagesSpider(Spider):
         selector = Selector(response=response)
         item = response.meta['item']
         # Extract product details
-        item['product_details'] = selector.xpath('//div[contains(@class, "description") and contains(@class, "stylenote")]/following-sibling::div[1]//text()[normalize-space()]').extract()[0]
+        item['product_details'] = selector.xpath('//div[contains(@class, "description") and contains(@class, "stylenote")]/following-sibling::div[1]//text()[normalize-space()]').extract()
         # Extract image urls
         item['image_urls'] = selector.xpath('//div[@class="thumbs"]/img/@data-blowup').extract()
         # Do ya thing!!
