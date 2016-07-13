c2e1731977c6f3340ce402e8251a8c4aef852708
Jithesh E J <mail@jithesh.net>
2016-07-13 18:03:45 +0530

    Adding (most of) the fields

--- myntra/items.py
+++ myntra/items.py
@@ -11,3 +11,12 @@ from scrapy import Item, Field
 class MyntraItem(Item):
     image_urls = Field()
     images = Field()
+    product = Field()
+    dre_landing_page_url = Field()
+    global_attr_article_type = Field()
+    visual_tag = Field()
+    global_attr_base_colour = Field()
+    gender_from_cms = Field()
+    styleid = Field()
+    product_additional_info = Field()
+    product_details = Field()
