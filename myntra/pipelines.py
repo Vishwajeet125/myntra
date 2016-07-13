57151037b3cc7cfe7f8b7b6c9a883f79aed9ce8a
Jithesh E J <mail@jithesh.net>
2016-07-13 19:47:05 +0530

    Adding pipeline to extract image paths

--- myntra/pipelines.py
+++ myntra/pipelines.py
@@ -6,6 +6,10 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class MyntraPipeline(object):
+class PathExtraction(object):
     def process_item(self, item, spider):
+        path = list()
+        for image in item['images']:
+            path.append(image['path'])
+        item['image_paths'] = ';'.join(path)
         return item
